01cdbc0d0875f7e8f7071d5f741b1e0f62309ba2
Arundhati Singh <arundhatisingh171003@gmail.com>
2024-09-03 09:45:46 -0500

    progress

--- object_tracker.py
+++ object_tracker.py
@@ -12,18 +12,25 @@ f_count = 0
 while True:
     ret, frame = captured_vid.read()
     f_count += 1
+    center_points = []
     if not ret or frame is None:
         break
     class_ids, scores, boxes = od.detect(frame)
     for box in boxes:
         (x, y, w, h) = box # the coordinates of the first point including the width and the height
+        # we are trying to get the center coordinates of the box within a single frame
+        center_x = int(x + x + w)//2
+        center_y = int(y + y + h)//2
+        center_points.append((center_x, center_y))
+        for pt in center_points: # draws a circle around the center of all the obiects irrespective of the frame they are present in for purposes of object tracking
+            cv2.circle(frame, (center_x, center_y), 5, (0, 0, 255), -1)
         print(f"Box coordinates - frame count {f_count}:", x, y, w, h)
         # we will form a rectangle around the object using these coordinates - cv2.rectangle takes top left and bottom right coordinates
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) 
 
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'): # reads the last 8 bits of the key pressed and compares it with the ascii value of 'q' 
+    if cv2.waitKey(0) & 0xFF == ord('q'): # reads the last 8 bits of the key pressed and compares it with the ascii value of 'q' 
         break
 
 
